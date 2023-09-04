b4427ff3791a26772fc3b5aa9259b231c8d67408
hyperst8 <huypn82@gmail.com>
2023-09-04 20:21:13 +0200

    Init commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>@copywrong 2023</div>
+      <div>Social media icons here</div>
+    </div>
+  );
+};
+
+export default Footer;
