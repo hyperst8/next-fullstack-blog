6a8051d50f24a94d5120defbc47b201be6f252c0
hyperst8 <huypn82@gmail.com>
2023-09-10 22:02:50 +0200

    Add Create and delete post, mutate revalidate

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,10 @@
 "use client";
 import useSWR from "swr";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./page.module.css";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export const metadata = {
   title: "Hyperst8 | Dashboard",
@@ -43,12 +44,16 @@ const Dashboard = () => {
   /** SWR method to fetch data **/
   const fetcher = (url) => fetch(url).then((res) => res.json());
 
-  const { data, error, isLoading } = useSWR(
-    "https://jsonplaceholder.typicode.com/posts",
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
     fetcher
   );
 
-  if (session.status === "loading") {
+  if (error) {
+    return <div>Something is wrong. Call for help</div>;
+  }
+
+  if (session.status === "loading" || isLoading) {
     return <div>loading...</div>;
   }
 
@@ -56,8 +61,84 @@ const Dashboard = () => {
     router.push("/dashboard/login");
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          img,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate(); // Revalidate if success. Don't have to manually refresh page
+      e.target.reset(); // Clear input fields after submit post
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (session.status === "authenticated") {
-    return <div className={styles.container}>Dashboard</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {data &&
+            data.length > 0 &&
+            data.map((post) => (
+              <div className={styles.post} key={post._id}>
+                <div className={styles.imgContainer}>
+                  <Image
+                    src={post.img}
+                    alt="blog image"
+                    width={200}
+                    height={100}
+                  />
+                </div>
+                <h2 className={styles.postTitle}>{post.title}</h2>
+                <span
+                  className={styles.delete}
+                  onClick={() => handleDelete(post._id)}
+                >
+                  X
+                </span>
+              </div>
+            ))}
+        </div>
+
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add new post</h1>
+          <input type="text" placeholder="Title" className={styles.input} />
+          <input type="text" placeholder="Desc" className={styles.input} />
+          <input type="text" placeholder="Image" className={styles.input} />
+          <textarea
+            placeholder="Content"
+            className={styles.textArea}
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
+    );
   }
 };
 
